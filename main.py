3d3476a04b834211eb44bc70df2d2738a3a098f3
Simon Botting <simon.botting@gmail.com>
2022-09-19 11:33:24 +0100

    added first commit of default file for this python project

---
+++ main.py
@@ -0,0 +1,23 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+###
+# VCS -> Enable Version Control Integration -> Git
+# Like doing git init at the command line
+# Then you've got a "Git" tab at the bottom and a "Commit" tab on the left hand side.
+# the add a .gitignore file to the root direct where the main.py file is
+# NOTE if you've already added some of the files that you didn't want to add
